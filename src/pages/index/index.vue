3025ce9b896278cf78eb85568d438d599a566d16
DESKTOP-P2JVFOB\DELL <zgdxst@qq.com>
2021-06-29 14:11:26 +0800

    init

---
+++ src/pages/index/index.vue
@@ -0,0 +1,25 @@
+<template>
+  <view class="content">
+    <view>
+      <text class="title">{{ title }}</text>
+    </view>
+  </view>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator';
+
+@Component
+export default class Index extends Vue {
+  title = 'Hello'
+
+  // eslint-disable-next-line class-methods-use-this
+  onLoad(): void {
+    console.log(process.env.NODE_ENV, process.env.VUE_APP_BASE_URL);
+  }
+}
+</script>
+
+<style lang="less" scoped>
+
+</style>
